7d83b39e38fbd7d243513049af73e97b3bb955c5
igao <igao@igaos-MacBook-Pro.local>
2023-07-22 12:04:55 -0300

    style: refactor comapnies component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,8 @@ import { ShortPresentation } from "@/components/ShortPresentation";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-900">
-      <ShortPresentation className="mb-4" />
-      <div className="flex justify-center items-center h-36 border-t border-b border-solid border-gray-700 bg-gray-800 mb-4">
-        <Companies />
-      </div>
+      <ShortPresentation />
+      <Companies />
     </div>
   );
 }
--- src/components/Companies/index.tsx
+++ src/components/Companies/index.tsx
@@ -1,25 +1,28 @@
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
 import companies from "./companies";
 
 interface CompaniesProps {
-  url: StaticImageData;
-  alt: string;
+  className?: string;
 }
 
-export const Companies = () => {
+export const Companies = ({ className }: CompaniesProps) => {
   return (
-    <div className="flex justify-center items-center flex-row gap-12 max-h-12 w-auto">
-      {companies.map((company) => (
-        <Image
-          className="grayscale w-12 h-auto"
-          key={company.id}
-          src={company.image}
-          alt={company.name}
-          width="0"
-          height="0"
-          sizes="100vw"
-        />
-      ))}
+    <div
+      className={`flex justify-center items-center h-36 border-t border-b border-solid border-gray-700 bg-gray-800 mb-4 ${className}`}
+    >
+      <div className="flex justify-center items-center flex-row gap-12 max-h-12 w-auto">
+        {companies.map((company) => (
+          <Image
+            className="grayscale w-12 h-auto"
+            key={company.id}
+            src={company.image}
+            alt={company.name}
+            width="0"
+            height="0"
+            sizes="100vw"
+          />
+        ))}
+      </div>
     </div>
   );
 };
