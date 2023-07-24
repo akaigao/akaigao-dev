af0fbe2f7b34b3de51c064a55d26268da25cf87a
igao <igao@igaos-MacBook-Pro.local>
2023-07-23 22:21:34 -0300

    fix: text ajust on services

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -1,12 +1,18 @@
 import { Layout, LayoutTemplate, Palette, Smartphone } from "lucide-react";
 import { ServicesCard } from "../ServicesCard";
+import { Button } from "../ui/button";
 
 export function Service() {
   return (
     <section className="h-auto w-auto p-1 mt-20 mx-4 text-center">
-      <h2 className="md:text-3xl text-2xl text-white lg:px-48 md:px-36">
-        Collaborate with brands and agencies to create impactful results.
+      <h2 className="md:text-3xl text-2xl text-white lg:px-48 md:px-36 mb-5">
+        Join forces with brands and agencies to achieve impactful outcomes.
       </h2>
+      <Button className="bg-transparent border border-zinc-800 hover:bg-zinc-800">
+        <a href="https://behance.net/akaigao" target="_blank">
+          See all projects
+        </a>
+      </Button>
       <ServicesCard.Root>
         <ServicesCard.Box>
           <ServicesCard.Icon icon={LayoutTemplate} />
@@ -20,22 +26,23 @@ export function Service() {
           <ServicesCard.Icon icon={Smartphone} />
           <ServicesCard.Title>Mobile App</ServicesCard.Title>
           <ServicesCard.Content>
-            Transforming ideas into exceptional web and mobile app experiences.
+            Turning ideas into extraordinary web and mobile app journeys.
           </ServicesCard.Content>
         </ServicesCard.Box>
         <ServicesCard.Box>
           <ServicesCard.Icon icon={Palette} />
           <ServicesCard.Title>Design & Creative</ServicesCard.Title>
           <ServicesCard.Content>
-            Crafting visually stunning designs that connect with your audience.
+            Crafting visually stunning designs that deeply connect with your
+            audience.
           </ServicesCard.Content>
         </ServicesCard.Box>
         <ServicesCard.Box>
           <ServicesCard.Icon icon={Layout} />
           <ServicesCard.Title>UX & UI</ServicesCard.Title>
           <ServicesCard.Content>
-            Designing interfaces that are intuitive, efficient, and enjoyable to
-            use.
+            Creating interfaces that are intuitive, efficient, and delightful to
+            navigate.
           </ServicesCard.Content>
         </ServicesCard.Box>
       </ServicesCard.Root>
