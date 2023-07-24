eee10747122201204b7cdc694aecd53b12429215
igao <igao@igaos-MacBook-Pro.local>
2023-07-23 21:51:28 -0300

    feat: new header composition

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,18 @@
+import Image from "next/image";
+import MyIconWhite from "../../../public/assets/svg/my-icon-white.svg";
 import { SocialMedias } from "../SocialMedias";
 
 export function Header() {
   return (
-    <header className="flex flex-row justify-between p-6 text-zinc-400">
-      <a
-        className="underline hover:text-zinc-200 transition"
-        href="mailto:akaigao.dev@gmail.com"
-      >
-        Hit me up
-      </a>
+    <header className="flex flex-row items-center justify-between p-6 lowercase tracking-widest text-zinc-400">
+      <div className="flex items-center justify-center w-14 h-14 rounded-full border border-zinc-700 border-opacity-30 bg-zinc-800 bg-opacity-30">
+        <Image
+          src={MyIconWhite}
+          alt="My personal icon"
+          width={30}
+          height={30}
+        />
+      </div>
       <SocialMedias.Root>
         <SocialMedias.Box
           href="https://www.linkedin.com/in/akaigao"
@@ -16,11 +20,9 @@ export function Header() {
         >
           <SocialMedias.Text>LinkedIn</SocialMedias.Text>
         </SocialMedias.Box>
-        <span>/</span>
         <SocialMedias.Box href="https://www.github.com/akaigao" target="_blank">
           <SocialMedias.Text>Github</SocialMedias.Text>
         </SocialMedias.Box>
-        <span>/</span>
         <SocialMedias.Box
           href="https://www.instagram.com/akaigao"
           target="_blank"
